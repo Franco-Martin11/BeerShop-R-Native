46c55579a19b01940e21ec0e7fae5905b4f8750b
Franco Martin <main.francomartin@gmail.com>
2023-12-25 22:48:47 -0300

    firs commit, placing scafolding from expo-reactNative

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Esto funcionara , es una prueba!</Text>
+      <Button title='esto es una prueba' onPress={() => alert('funcionara')}/>
       <StatusBar style="auto" />
     </View>
   );
